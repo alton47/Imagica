d1b47063c1ad51a42b988c18a63c12194f6d6f89
Allan <altonallan47@gmail.com>
2023-03-10 18:49:22 +0100

    Update FormField.jsx

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const FormField = ({ labelName, type, name, placeholder, value, handleChange, isSupriseMe, handleSupriseMe }) => {
+const FormField = ({ labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
   return (
     <div>
-        <div className='flex items-center gap-2 mb-2'></div>
+        <div className='flex items-center gap-2 mb-2'>
+            <label 
+            htmlFor={name}
+            className='block text-sm font-medium text-gray-900'
+            >
+                {labelName}
+            </label>
+        </div>
     </div>
   )
 }
